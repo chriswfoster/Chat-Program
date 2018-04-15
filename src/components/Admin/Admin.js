46ef46be24c2b3148d9af23e481fdd32e64dc0b7
ChriswFoster <chriswfoster@gmail.com>
2018-04-14 23:53:58 -0500

    Bringing in key in admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,20 +1,55 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import ImageGallery from "react-image-gallery"
+
+import { getProductKey } from "../../redux/reducer"
+
+import "./admin.css"
+import "react-image-gallery/styles/css/image-gallery.css"
 
 class Admin extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            itemsArray:[]
 
+        }
+    }
+        componentDidMount() {
+          this.props.getProductKey()
+        }
 
-render() {
-console.log(this.props)
-return(
-<div>
-    <button>Add random public chat</button>
+        createRandomChate(){
+            
+        }
 
-    <div>
+  render() {
+    console.log(this.props)
+   
+    
+    return (
+      <div style={{ width: "100vw" }}>
+        <button>Add random public chat</button>
 
+        <div style={{ width: "70vw" }} className="Carousel">
+          
+          <form onSubmit={() => this.submitIt()}>
+              <input type="text" />
+              <input type="submit" />
+              </form>
+          
+          
+          {/* <ImageGallery
+            items={images}
+            showFullscreenButton={false}
+            showPlayButton={false}
+          /> */}
         </div>
-
-</div>
-)}
+      </div>
+    )
+  }
 }
-export default Admin
+const mapStateToProps = state => state
+export default connect(mapStateToProps, {
+getProductKey
+})(Admin)
